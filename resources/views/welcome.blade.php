e9985eec1844e28af2bc09d39426c136aa995e61
Oscar Daniel <saito_dan@outlook.com>
2019-11-01 09:55:37 -0600

    laravel welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello Laravel!
+                    ...Hello Laravel!
                 </div>
 
                 <div class="links">
